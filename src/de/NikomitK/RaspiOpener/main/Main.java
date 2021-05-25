f97b78e83beaae671b05a29123ce5a7156a1cbee
Chaoscaot <chaoscaot444@gmail.com>
2021-05-25 16:07:38 +0200

    Refactor main-Method

--- src/de/NikomitK/RaspiOpener/main/Main.java
+++ src/de/NikomitK/RaspiOpener/main/Main.java
@@ -1,52 +1,58 @@
 package de.NikomitK.RaspiOpener.main;
 
-import de.NikomitK.RaspiOpener.handler.*;
-import java.io.*;
+import de.NikomitK.RaspiOpener.handler.Printer;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
 
 public class Main {
+
     private static final StringWriter sw = new StringWriter();
     private static final PrintWriter pw = new PrintWriter(sw);
     private static final DateFormat dateF = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    public static File logFile;
+    public static File debugLog;
+    public static File keyPasStore;
+    public static File otpStore;
+    public static File nonceStore;
+
     public static void main(String[] args) throws Exception {
-        String logfileName = "log.txt";
+        logFile = new File("log.txt");
+        debugLog = new File("debugLog.txt");
+        keyPasStore = new File("keyPasStore.txt");
+        otpStore = new File("otpStore.txt");
+        nonceStore = new File("nonceStore.txt");
         boolean debug = false;
         if(args.length>0 && args[0].equals("-r")){
-            BashIn.exec("sudo rm keyPasStore.txt");
+            if(keyPasStore.exists()) {
+                keyPasStore.delete();
+            }
         }
         else if(args.length>0 && args[0].equals("-debug")) {
             debug = true;
-            logfileName = "debugLog.txt";
         }
 //        gets removed in the next update, just here in case the replacement doesn't work reliably
-        try{
-            Scanner sc = new Scanner(new File("keyPasStore.txt"));
+        if(!keyPasStore.exists()) {
+            keyPasStore.createNewFile();
         }
-        catch (FileNotFoundException e){
-            BashIn.exec("sudo touch keyPasStore.txt");
+        if(!otpStore.exists()) {
+            otpStore.createNewFile();
         }
-        try{
-            Scanner sc = new Scanner(new File("otpStore.txt"));
+        if(!nonceStore.exists()) {
+            nonceStore.createNewFile();
         }
-        catch(FileNotFoundException e){
-            BashIn.exec("sudo touch otpStore.txt");
-        }
-        try{
-            Scanner sc = new Scanner(new File("nonceStore.txt"));
-        }
-        catch(FileNotFoundException e){
-            BashIn.exec("sudo touch nonceStore.txt");
-        }
-//        new File("keyPasStore.txt").createNewFile();
-//        new File("otpStore.txt").createNewFile();
-//        new File("nonceStore.txt").createNewFile();
+        //new File("keyPasStore.txt").createNewFile();
+        //new File("otpStore.txt").createNewFile();
+        //new File("nonceStore.txt").createNewFile();
         try{
             System.out.println("Starting...");
             // TCP Server starten...
-            TCPServer.run(logfileName, debug);
+            TCPServer.run(logFile.getName(), debug);
         }
         catch(Exception e){
             e.printStackTrace(pw);
