e306d201921a1fb63b3d8b755d61ccb687a28156
yoyosource <yoyosource@nidido.de>
2021-05-25 17:18:33 +0200

    Update GpioUtils

---
+++ src/de/NikomitK/RaspiOpener/handler/GpioUtils.java
@@ -0,0 +1,24 @@
+package de.NikomitK.RaspiOpener.handler;
+
+import com.pi4j.io.gpio.*;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class GpioUtils {
+
+    private final GpioController gpio = GpioFactory.getInstance();
+    private final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLed", PinState.LOW);
+
+    // gets called from de.NikomitK.RaspiOpener.main.TCPServer, if the App sends a command to activate GPIO pin 25
+    public void activate(long time) {
+        // sets the previously specified pin from 0V to 3.3V and back
+        pin.high();
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        pin.low();
+    }
+}
+
