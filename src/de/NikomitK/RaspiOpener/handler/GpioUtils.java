d90752fff03d70ed786a8de54b777555af2556b9
Zeanon <thezeanon@gmail.com>
2021-05-25 21:10:28 +0200

    Logger

--- src/de/NikomitK/RaspiOpener/handler/GpioUtils.java
+++ src/de/NikomitK/RaspiOpener/handler/GpioUtils.java
@@ -21,4 +21,3 @@ public class GpioUtils {
         pin.low();
     }
 }
-
---
+++ src/de/NikomitK/RaspiOpener/handler/Logger.java
@@ -0,0 +1,83 @@
+package de.NikomitK.RaspiOpener.handler;
+
+import de.NikomitK.RaspiOpener.main.Main;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Logger {
+
+    private final BufferedOutputStream outputStream;
+    private final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    public Logger(File file) throws FileNotFoundException {
+        outputStream = new BufferedOutputStream(new FileOutputStream(file, true));
+    }
+
+    public void log(String message) {
+        try {
+            internalLog(dateFormat.format(new Date()) + " [LOG  ] " + message + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void debug(String message) {
+        if (!Main.isDebug()) {
+            return;
+        }
+
+        try {
+            internalLog(dateFormat.format(new Date()) + " [DEBUG] " + message + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void warn(Exception exception) {
+        try {
+            internalLog(dateFormat.format(new Date()) + " [WARN ] " + exception.toString() + "\n");
+
+            Set<Throwable> dejaVu = new HashSet<>();
+            dejaVu.add(exception);
+
+            internalPrint(dejaVu, exception.getStackTrace(), exception.getSuppressed(), exception.getCause());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void internalPrint(Set<Throwable> dejaVu, StackTraceElement[] stackTrace, Throwable[] suppressed, Throwable cause) throws IOException {
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            internalLog("\tat " + stackTraceElement);
+        }
+
+        for (Throwable throwable : suppressed) {
+            internalWarn(throwable, dejaVu, "Suppressed", "\t");
+        }
+
+        if (cause != null) {
+            internalWarn(cause, dejaVu, "Caused by", "");
+        }
+    }
+
+    private void internalWarn(Throwable throwable, Set<Throwable> dejaVu, String suppressedPrefix, String prefix) throws IOException {
+        if (dejaVu.contains(throwable)) {
+            return;
+        }
+
+        dejaVu.add(throwable);
+        internalLog(prefix + dateFormat.format(new Date()) + " [WARN ] [" + suppressedPrefix + "] " + throwable.toString() + "\n");
+
+        internalPrint(dejaVu, throwable.getStackTrace(), throwable.getSuppressed(), throwable.getCause());
+    }
+
+    private void internalLog(String message) throws IOException {
+        outputStream.write(message.getBytes(StandardCharsets.UTF_8));
+    }
+}
