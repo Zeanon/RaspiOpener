2ad911aed442ef49cdae2fbb5b9f3d898794275a
yoyosource <yoyosource@nidido.de>
2021-05-25 17:01:45 +0200

    Update Error with comments

--- src/de/NikomitK/RaspiOpener/handler/Error.java
+++ src/de/NikomitK/RaspiOpener/handler/Error.java
@@ -9,15 +9,24 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum Error {
 
+    // Everything is OK
     OK,
+
+    // Anything related with the key
     KEY_UNSAVED("01"),
-    PASSWORD_EXISTS("02"),
     KEY_MISMATCH("03"),
-    OTP_NOT_FOUND("04"),
+
+    // Anything related with the password
+    PASSWORD_EXISTS("02"),
     PASSWORD_MISMATCH("05"),
     PASSWORD_NOT_SAVED("06"),
     PASSWORD_NO_RESET("07"),
+
+    // Anything related with the OneTimePad
+    OTP_NOT_FOUND("04"),
     OPT_NOT_SAVED("08"),
+
+    // Anything else
     SERVER_ERROR("09"),
     COMMAND_WRONG("10");
 
