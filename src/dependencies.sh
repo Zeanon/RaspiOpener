a5e916389d34b8572f277836b2febbf7fad42456
yoyosource <yoyosource@nidido.de>
2021-05-26 20:03:01 +0200

    Update dependencies.sh

--- src/dependencies.sh
+++ src/dependencies.sh
@@ -2,7 +2,7 @@
 
 # sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install wiringpi -y
-# sudo apt-get install screen -y
+sudo apt-get install screen -y
 # sudo apt-get install git -y
 sudo apt-get install bluetooth -y
 sudo apt-get install pi-bluetooth -y
