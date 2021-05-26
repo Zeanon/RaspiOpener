0fa0c664b02cc962e2b62ea2b2f2b8b2b87bc787
yoyosource <yoyosource@nidido.de>
2021-05-26 11:31:38 +0200

    Add Storage

---
+++ src/de/NikomitK/RaspiOpener/handler/Storage.java
@@ -0,0 +1,20 @@
+package de.NikomitK.RaspiOpener.handler;
+
+import lombok.Getter;
+import lombok.ToString;
+import yapion.annotations.object.YAPIONData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@YAPIONData
+@Getter
+@ToString
+public class Storage {
+
+    private String key;
+    private String hash;
+    private String nonce;
+    private List<String> otps = new ArrayList<>();
+
+}
