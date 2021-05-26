5ce602672aa24232b06417d2431ad29c97cc76f0
yoyosource <yoyosource@nidido.de>
2021-05-26 18:10:21 +0200

    Add Main.loadFileFromJar

--- src/de/NikomitK/RaspiOpener/main/Main.java
+++ src/de/NikomitK/RaspiOpener/main/Main.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import yapion.parser.YAPIONParser;
 import yapion.serializing.YAPIONDeserializer;
 
-import java.io.File;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class Main {
@@ -29,6 +31,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         logger = new Logger(new File("log.txt"));
+
+        loadFileFromJar("dependencies.sh", true);
+        loadFileFromJar("bluetooth.sh", false);
 
         logger.log("Loading Storage");
         storageFile = new File("storage.yapion");
@@ -89,5 +94,44 @@ public class Main {
         logger.debug("Resetting Storage");
         storage = new Storage();
         storage.save();
+    }
+
+    private static void loadFileFromJar(String fileName, boolean exec) throws IOException, NoSuchAlgorithmException {
+        File file = new File(fileName);
+        if (file.exists()) {
+            byte[] bytes1 = md5(Main.class.getResourceAsStream("/" + fileName));
+            byte[] bytes2 = md5(new FileInputStream(file));
+            if (Arrays.equals(bytes1, bytes2)) {
+                logger.log("File already exists");
+                return;
+            }
+            logger.log("Update File as it has changed or has been changed");
+        }
+        InputStream inputStream = Main.class.getResourceAsStream("/" + fileName);
+        if (inputStream == null) {
+            logger.warn("File in Jar not found");
+            System.exit(1);
+        }
+        inputStream.transferTo(new FileOutputStream(file));
+        logger.debug(file.length() + " bytes loaded");
+
+        if (exec) {
+            logger.debug("Setting: " + fileName + " executable");
+            file.setExecutable(true);
+            logger.debug("Running: bash -c ./" + fileName);
+            Runtime.getRuntime().exec("bash -c", new String[]{"./" + fileName});
+        }
+    }
+
+    private static byte[] md5(InputStream inputStream) throws IOException, NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        while (inputStream.available() > 0) {
+            int i = inputStream.read();
+            if (i == -1) {
+                break;
+            }
+            messageDigest.update((byte) i);
+        }
+        return messageDigest.digest();
     }
 }
