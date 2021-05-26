4896dc58fd6902c24be175aa18fb248c8d095ea4
yoyosource <yoyosource@nidido.de>
2021-05-26 18:41:49 +0200

    Update dependencies.sh

--- src/dependencies.sh
+++ src/dependencies.sh
@@ -4,7 +4,7 @@
 sudo apt-get install wiringpi -y
 # sudo apt-get install screen -y
 # sudo apt-get install git -y
-sudo apt-get install bluetooth
-sudo apt-get install pi-bluetooth
-sudo apt-get install bluez
-sudo apt-get install blueman
+sudo apt-get install bluetooth -y
+sudo apt-get install pi-bluetooth -y
+sudo apt-get install bluez -y
+sudo apt-get install blueman -y
